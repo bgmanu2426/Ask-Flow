f897c7a26dc7817c9223c41161334b95b4c28218
manubg <bgmanu2426@gmail.com>
2024-11-18 13:13:31 +0530

    Fix HeroSection thumbnail reference, remove unused Home component code, and implement vote API route for managing user votes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HeroParallax } from "@/components/ui/hero-parallax";
 import { databases } from "@/models/server/config";
-import { db, questionAttachmentBucket, questionCollection } from "@/models/name";
+import { db, questionAttachmentsBucket, questionCollection } from "@/models/name";
 import { Query } from "node-appwrite";
 import slugify from "@/utils/slugify";
 import { storage } from "@/models/client/config";
@@ -19,7 +19,7 @@ export default async function HeroSection() {
             products={questions.documents.map(q => ({
                 title: q.title,
                 link: `/questions/${q.$id}/${slugify(q.title)}`,
-                thumbnail: storage.getFilePreview(questionAttachmentBucket, q.attachmentId).href,
+                thumbnail: storage.getFilePreview(questionAttachmentsBucket, q.attachmentId).href,
             }))}
         />
     );
