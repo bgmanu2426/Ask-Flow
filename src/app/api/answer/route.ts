cbe8e94b67e83c7711520654747a44b79ea58425
manubg <bgmanu2426@gmail.com>
2024-11-18 11:26:42 +0530

    Refactor layout structure, update utility imports, and enhance styling; add API routes for answer management

---
+++ src/app/api/answer/route.ts
@@ -0,0 +1,48 @@
+import { answerCollection, db } from "@/models/name";
+import { databases, users } from "@/models/server/config";
+import { NextRequest, NextResponse } from "next/server";
+import { ID } from "node-appwrite";
+import { UserPrefs } from "@/store/Auth";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { questionId, answer, authorId } = await request.json();
+        const response = await databases.createDocument(db, answerCollection, ID.unique(), {
+            content: answer,
+            authorId,
+            questionId
+        })
+
+        // Increase the authors reputation
+        const prefs = await users.getPrefs<UserPrefs>(authorId);
+        await users.updatePrefs(authorId, {
+            reputation: Number(prefs.reputation) + 1
+        });
+
+        return NextResponse.json({ response }, { status: 201 });
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error?.message || "Error creating the answer" },
+            { status: error?.status || error?.code || 500 });
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    try {
+        const { answerId } = await request.json();
+        const answer = await databases.getDocument(db, answerCollection, answerId);
+        const response = await databases.deleteDocument(db, answerCollection, answerId);
+
+        // Decrease the authors reputation
+        const prefs = await users.getPrefs<UserPrefs>(answer.authorId);
+        await users.updatePrefs(answer.authorId, {
+            reputation: Number(prefs.reputation) - 1
+        });
+
+        return NextResponse.json({ response }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error?.message || "Error deleting the answer" },
+            { status: error?.status || error?.code || 500 });
+    }
+}
